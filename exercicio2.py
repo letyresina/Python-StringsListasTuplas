78c121b5f27e6664daf57ba6689fda13ed187212
letyresina <leticiaresina@gmail.com>
2023-08-18 11:27:54 -0300

    Subindo exercício 2

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+'''
+    Crie uma função que receba uma string e retorne a mesma string com todas as letras em 
+    maiúsculas.
+'''
+
+def recebeString(a):
+    return a.upper()
+
+print(recebeString("The Beatles"))
