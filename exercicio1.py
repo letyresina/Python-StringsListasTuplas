0d45655128dbedd912e2684d88fc5487cb382aa1
letyresina <leticiaresina@gmail.com>
2023-08-18 11:25:39 -0300

    Subindo documentação e exercício1

---
+++ exercicio1.py
@@ -0,0 +1,9 @@
+'''
+    Exercício 1
+    Escreva uma função que receba uma string e retorne o número de caracteres nela.
+'''
+
+def recebeString(a):
+    return len(a)
+
+print(recebeString("Como diriam os Beatles: Help!"))
